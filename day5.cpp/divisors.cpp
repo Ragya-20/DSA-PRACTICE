d8ee6a152b234667ee24b496ab1b598ff0d621e7
Ragya <ragya005@gmail.com>
2025-03-31 13:16:13 +0530

    update divisors.cpp

--- day5.cpp/divisors.cpp
+++ day5.cpp/divisors.cpp
@@ -1,3 +1,4 @@
+//by checking for 1 to n
 #include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
@@ -12,5 +13,28 @@ int main(){
     int n;
     cin>>n;
     printDivisors(n);
+    return 0;
+}
+//by checking for 1 to root n by vector method
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+void printdivisors(int n){
+    vector<int>ls;
+    for(int i=1;i<=sqrt(n);i++){
+        if(n%i==0){
+            ls.push_back(i);
+            if((n/i)!=i){
+                ls.push_back(n/i);
+            }
+        }
+    }
+    sort(ls.begin(),ls.end());
+    for(auto it:ls) cout<<it<<" ";
+}
+int main(){
+    int n;
+    cin>>n;
+    printdivisors(n);
     return 0;
 }
