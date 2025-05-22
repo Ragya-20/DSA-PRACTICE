6a6380525c7c3c76651e46d9db1476aab9ed6cda
Ragya <ragya005@gmail.com>
2025-05-23 00:19:59 +0530

    Create recursiveinsertionsort.cpp
    recursive insertionsort

---
+++ day9/recursiveinsertionsort.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int arr[], int i, int n) {
+    if (i == n) return;
+
+    int j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+        int temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+        j--;
+    }
+
+    insertionSort(arr, i + 1, n);
+}
