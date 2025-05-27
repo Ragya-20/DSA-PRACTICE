293240b86690ae800a84dc42ded468864d46f5ed
Ragya <ragya005@gmail.com>
2025-05-27 23:53:28 +0530

    Create 2894.cpp

---
+++ day14/2894.cpp
@@ -0,0 +1,23 @@
+/*You are given positive integers n and m.
+
+Define two integers as follows:
+
+num1: The sum of all integers in the range [1, n] (both inclusive) that are not divisible by m.
+num2: The sum of all integers in the range [1, n] (both inclusive) that are divisible by m.
+Return the integer num1 - num2.*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int num1=0;
+        int num2=0;
+        for(int i=1;i<=n;i++){
+            if(i%m==0) num2+=i;
+            else num1+=i;
+        }
+        return num1-num2;
+    }
+};
